79a28b69795531aff4ddc28da075c59b48022791
MoeAlAnnan <5653@holbertonstudents.com>
2024-01-24 20:28:36 +0000

    output fix

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,18 +1,22 @@
 #!/usr/bin/node
+
 const request = require('request');
 const fs = require('fs');
+
+if (process.argv.length !== 4) {
+  process.exit(1);
+}
+
 const url = process.argv[2];
 const filePath = process.argv[3];
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8', (writeError) => {
-      if (writeError) {
-        console.error(writeError);
-      } else {
-        console.log(`Body response written to ${filePath}`);
-      }
-    });
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.error('error:', err);
   }
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 });
