f96fca9626d26d78e13afdbad7b9930840e372a6
MoeAlAnnan <5653@holbertonstudents.com>
2024-01-26 22:58:49 +0000

    read

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-if (process.argv.length !== 3) {
-  process.exit(1);
-}
-
 const filePath = process.argv[2];
-
 fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
